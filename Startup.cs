898eb895380373b20e1a87da801048c30000b132
ChinChuanKuo <chinchuankuo@rickchu1.flytech.com.tw>
2020-12-24 10:01:20 +0800

    module better

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace userinfoApi
                 options.AddPolicy("Forget",
                     builder => builder.AllowAnyOrigin().AllowAnyHeader().WithMethods("POST"));
                 options.AddPolicy("Login",
-                    builder => builder.WithOrigins(new corsorigins().connectionString()).AllowAnyHeader().WithMethods("POST"));
+                    builder => builder.WithOrigins("http://221.222.222.181:7250", "http://localhost:5000").AllowAnyHeader().WithMethods("POST"));
                 options.AddPolicy("Resend",
                     builder => builder.AllowAnyOrigin().AllowAnyHeader().WithMethods("POST"));
                 options.AddPolicy("Signup",
